c6a8a4b23149033f94a41ff2d9656a15339cc670
8ball030 <8baller@station.codes>
2025-11-27 17:47:47 +0000

    feat:added-simple-table

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,17 @@
 <!-- We add in the background and center everything -->
 <script lang="ts">
-	import Counter from "../components/counter.svelte";
+	import Counter from '../components/counter.svelte';
+	import Table from '../components/parameterTable.svelte';
 </script>
 
-<main class="flex min-h-screen flex-col items-center justify-center bg-primary p-4 text-primary-foreground">
+<main
+	class="flex min-h-screen flex-col items-center justify-center gap-6 bg-primary p-4 text-primary-foreground"
+>
 	<h1>Welcome to SvelteKit</h1>
 	<p>
 		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
 	</p>
 
 	<Counter />
+	<Table />
 </main>
